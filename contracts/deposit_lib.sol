22ba32a606dc99acd21c1fc038c793e39ad65a80
Eldaniz Babayev <eldanizbabayev@protonmail.com>
2022-06-08 23:16:16 +0400

    multiple deposit deploy

--- contracts/deposit_lib.sol
+++ contracts/deposit_lib.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 library deposit {
 
-    function calc(uint256 ay, uint256 pul) public pure returns(uint256) {
-        uint256 value = pul + (pul - ((5 * ay) / 100));
+    function calc(uint256 ay, uint256 pul, uint256 faiz) public pure returns(uint256) {
+        uint256 value = pul + (pul - ((faiz * ay) / 100));
 
         return value;
     }
--- test/sample-test.js
+++ test/sample-test.js
@@ -4,9 +4,18 @@ describe("deposit_lib", function () {
   it("deposit library explained :D", async function () {
     const lib_factory = await ethers.getContractFactory("Deposit", {libraries: {
 
-      deposit: lib_factory.adress()
+      deposit: "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
     }});
 
     const deposit = await lib_factory.deploy(8, 900);
+  });
+
+  it("deposit library explained :D", async function () {
+    const lib_factory2 = await ethers.getContractFactory("Deposit2", {libraries: {
+
+      deposit: "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"
+    }});
+
+    const deposit2 = await lib_factory2.deploy(8, 900);
   });
 });
